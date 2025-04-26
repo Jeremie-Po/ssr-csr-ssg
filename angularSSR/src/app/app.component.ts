6b3b6b8bc94685963c113e44081f44ea5389a542
Jérémie P <jpourageaux@hotmail.fr>
2025-04-26 18:12:38 +0200

    afterrender after next render

--- angularSSR/src/app/app.component.ts
+++ angularSSR/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {afterNextRender, afterRender, Component, viewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,26 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angularSSR';
+  ref = viewChild('test');
+
+  constructor(){
+    // dans le cas d'un ssr, il n'est pas possible d'acceder aux webApi (localStorage,window.qlequchose)
+    // car le fichier sera généréer coté(serveur)
+    // par contre il est possible de donner des instructions qui s'executeront lorsque la' +
+    // page web aura été rendu coté client
+
+    // ne s execute qu"une fois"
+    afterRender(()=>{
+    console.log(this.ref()); //sera executé 3 fois
+
+    })
+
+    // s execute autant que la page est rendue÷
+    afterNextRender(()=>{
+    localStorage.setItem('picsou','Donald');
+      console.log('afternextrender',this.ref()); //sera executé 1 fois
+
+
+    })
+  }
 }
