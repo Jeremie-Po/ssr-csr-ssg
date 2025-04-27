dc4683528163fbd6725b597b670c713e78ad510d
Jérémie P <jpourageaux@hotmail.fr>
2025-04-27 10:50:10 +0200

    @defer permet d hydrater sur le chargement initial

--- angularSSR/src/app/app.component.ts
+++ angularSSR/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {afterNextRender, afterRender, Component, viewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
+import {AComponent} from './a/a.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NgOptimizedImage],
+  imports: [RouterOutlet, NgOptimizedImage, AComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
